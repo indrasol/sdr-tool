60be18bc2fa670042d44c1341c107bddb870ba58
Rithin25295 <Rithin25295@gmail.com>
2025-03-11 19:48:01 -0500

    updated backend

--- sdr_backend/v1/api/routes/routes.py
+++ sdr_backend/v1/api/routes/routes.py
@@ -15,10 +15,10 @@ from v1.api.routes.register.auth import router as auth_router
 from v1.api.routes.model_with_ai.design import router as design_router
 from v1.api.routes.model_with_ai.dummy_design import router as dummy_design_router
 # Model With AI - Reports
-from v1.api.routes.model_with_ai.reports.generate_report import router as generate_report_router
-from v1.api.routes.model_with_ai.reports.update_report import router as update_report_router
-from v1.api.routes.model_with_ai.reports.get_report import router as get_report_router
-from v1.api.routes.model_with_ai.reports.get_report_versions import router as get_report_versions_router
+# from v1.api.routes.model_with_ai.reports.generate_report import router as generate_report_router
+# from v1.api.routes.model_with_ai.reports.update_report import router as update_report_router
+# from v1.api.routes.model_with_ai.reports.get_report import router as get_report_router
+# from v1.api.routes.model_with_ai.reports.get_report_versions import router as get_report_versions_router
 
 # Projects
 from v1.api.routes.projects.projects import router as project_router
@@ -49,10 +49,10 @@ router.include_router(design_router, tags=["model_with_ai - design_router"])
 router.include_router(dummy_design_router, tags=["model_with_ai - dummy_design_router"])
 
 # Model With AI - Reports
-router.include_router(generate_report_router, tags=["model_with_ai - generate_report_router"])
-router.include_router(update_report_router, tags=["model_with_ai - update_report_router"])
-router.include_router(get_report_router, tags=["model_with_ai - get_report_router"])
-router.include_router(get_report_versions_router, tags=["model_with_ai - get_report_versions_router"])
+# router.include_router(generate_report_router, tags=["model_with_ai - generate_report_router"])
+# router.include_router(update_report_router, tags=["model_with_ai - update_report_router"])
+# router.include_router(get_report_router, tags=["model_with_ai - get_report_router"])
+# router.include_router(get_report_versions_router, tags=["model_with_ai - get_report_versions_router"])
 
 
 # Projects
