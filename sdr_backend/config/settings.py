b5c29c5211ed02ec48a8de82f009bbd2dbe28fab
Rithin25295 <64233479+Rithin25295@users.noreply.github.com>
2025-03-17 01:04:33 -0500

    Update settings.py

--- sdr_backend/config/settings.py
+++ sdr_backend/config/settings.py
@@ -89,6 +89,6 @@ HEALTH_API_KEY = os.getenv("HEALTH_API_KEY")
 # Redis settings for session management
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
-REDIS_PASSWORD = ""
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 REDIS_DB = os.getenv("REDIS_DB")
 SESSION_EXPIRY = 86400
